31cb232a5f6df4c914ca2ede19bd4d4654b9aa33
Fahad Ameen <fahad.ameen@northbaysolutions.net>
2022-09-29 22:11:02 +0500

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { FahadLibSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  FahadLibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <fahad-lib-sample />
+  </div>
+</template>
